5713305e97fd55be66b67eed5e25de88543bc933
Julien Eberle <julien.eberle@nestle.com>
2016-10-13 11:13:58 +0200

    remove tests duplications with shared_examples

--- spec/classes/port389_spec.rb
+++ spec/classes/port389_spec.rb
@@ -12,8 +12,8 @@ describe 'port389', :type => :class do
   end
 
   describe 'on osfamily RedHat' do
-    describe 'on OS version 6' do
-      let(:facts) {{ :osfamily => 'RedHat', :os => 'Linux', :operatingsystemmajrelease => '6', :operatingsystemrelease => '6' }}
+
+    shared_examples 'port389 examples' do
 
       redhat_packages = [
         '389-admin',
@@ -49,7 +49,7 @@ describe 'port389', :type => :class do
           })
         end
         it 'should enable dirsrv' do
-          should contain_service('dirsrv').with({
+          should contain_service(:service_params[:name]).with({
             :ensure => 'running',
             :enable => true,
           }).that_comes_before('Anchor[port389::end]')
@@ -97,7 +97,7 @@ describe 'port389', :type => :class do
           it { should_not contain_class('port389::tune') }
             it do
             # XXX implimentation specific
-            should contain_service('dirsrv').with({
+            should contain_service(:service_params[:name]).with({
               :ensure => 'stopped',
               :enable => false,
             }).that_comes_before('Class[port389::install]')
@@ -114,7 +114,7 @@ describe 'port389', :type => :class do
           it { should_not contain_class('port389::tune') }
           it do
             # XXX implimentation specific
-            should contain_service('dirsrv').with({
+            should contain_service(:service_params[:name]).with({
               :ensure => 'stopped',
               :enable => false,
             }).that_comes_before('Class[port389::install]')
@@ -183,180 +183,19 @@ describe 'port389', :type => :class do
           end
         end # []
       end # enable_tuning =>
-    end # on os version 6
+    end # shared_example
+
+    describe 'on OS version 6' do
+      let(:facts) {{ :osfamily => 'RedHat', :os => 'Linux', :operatingsystemmajrelease => '6', :operatingsystemrelease => '6' }}
+      let(:service_params) {{ :name => 'dirsrv' }}
+      include_examples 'port389 examples'
+    end # os version 6
 
     describe 'on OS version 7' do
       let(:facts) {{ :osfamily => 'RedHat', :os => 'Linux', :operatingsystemmajrelease => '7', :operatingsystemrelease => '7' }}
-
-      redhat_packages = [
-        '389-admin',
-        '389-admin-console',
-        '389-admin-console-doc',
-        #'389-admin-debuginfo',
-        '389-adminutil',
-        #'389-adminutil-debuginfo',
-        '389-adminutil-devel',
-        '389-console',
-        '389-ds',
-        '389-ds-base',
-        '389-ds-base-devel',
-        '389-ds-base-libs',
-        '389-ds-console',
-        '389-ds-console-doc',
-        '389-dsgw',
-        #'389-dsgw-debuginfo',
-      ]
-
-      context 'param defaults' do
-        it { should_not contain_class('port389::tune') }
-        it('should include package dependency') { should contain_package('httpd') }
-        redhat_packages.each do |pkg|
-          it('should include package') { should contain_package(pkg) }
-        end
-        it 'should manage setup dir' do
-          should contain_file('/var/lib/dirsrv/setup').with({
-            :ensure => 'directory',
-            :owner  => 'nobody',
-            :group  => 'nobody',
-            :mode   => '0700',
-          })
-        end
-        it 'should enable dirsrv.target' do
-          should contain_service('dirsrv.target').with({
-            :ensure => 'running',
-            :enable => true,
-          }).that_comes_before('Anchor[port389::end]')
-        end
-      end # param defaults
-
-      context 'ensure =>' do
-        context 'present' do
-          let(:params) {{ :ensure => 'present' }}
-
-          it { should_not contain_class('port389::tune') }
-          redhat_packages.each do |pkg|
-            it { should contain_package(pkg).with_ensure('present') }
-          end
-          it do
-            should contain_file('/var/lib/dirsrv/setup').with({
-              :ensure => 'directory',
-              :owner  => 'nobody',
-              :group  => 'nobody',
-              :mode   => '0700',
-            })
-          end
-        end # present
-
-        context 'latest' do
-          let(:params) {{ :ensure => 'latest' }}
-
-          it { should_not contain_class('port389::tune') }
-            redhat_packages.each do |pkg|
-            it { should contain_package(pkg).with_ensure('latest') }
-          end
-          it do
-            should contain_file('/var/lib/dirsrv/setup').with({
-              :ensure => 'directory',
-              :owner  => 'nobody',
-              :group  => 'nobody',
-              :mode   => '0700',
-            })
-          end
-        end # latest
-
-        context 'absent' do
-          let(:params) {{ :ensure => 'absent' }}
-
-          it { should_not contain_class('port389::tune') }
-            it do
-            # XXX implimentation specific
-            should contain_service('dirsrv.target').with({
-              :ensure => 'stopped',
-              :enable => false,
-            }).that_comes_before('Class[port389::install]')
-          end
-          redhat_packages.each do |pkg|
-            it { should contain_package(pkg).with_ensure('absent') }
-          end
-          it { should_not contain_file('/var/lib/dirsrv/setup') }
-        end # absent
-
-        context 'purged' do
-          let(:params) {{ :ensure => 'purged' }}
-
-          it { should_not contain_class('port389::tune') }
-          it do
-            # XXX implimentation specific
-            should contain_service('dirsrv.target').with({
-              :ensure => 'stopped',
-              :enable => false,
-            }).that_comes_before('Class[port389::install]')
-          end
-          redhat_packages.each do |pkg|
-            it { should contain_package(pkg).with_ensure('absent') }
-          end
-          [
-            'rm -f /etc/sysconfig/dirsrv*',
-            'rm -rf /etc/dirsrv/',
-            'rm -rf /usr/lib64/dirsrv/',
-            'rm -rf /var/log/dirsrv/',
-            'rm -rf /var/lib/dirsrv/',
-            'rm -rf /var/lock/dirsrv/',
-            'rm -rf /usr/share/dirsrv/',
-            'rm -f /etc/selinux/targeted/modules/active/modules/dirsrv-admin.pp',
-            'rm -f /etc/selinux/targeted/modules/active/modules/dirsrv.pp',
-            'rm -f /usr/share/selinux/devel/include/services/dirsrv-admin.if',
-            'rm -f /usr/share/selinux/devel/include/services/dirsrv.if',
-            'rm -f /usr/share/selinux/targeted/dirsrv-admin.pp.bz2',
-            'rm -f /usr/share/selinux/targeted/dirsrv.pp.bz2',
-          ].each do |cmd|
-            it { should contain_exec(cmd) }
-          end
-          it do
-            # XXX  implimentation specific
-            should contain_file('/var/lib/dirsrv/setup').with({
-             :ensure => 'absent',
-             :force  => true,
-            }).that_requires('Class[port389::install]')
-          end
-        end # purged
-
-        context 'foo' do
-          let(:params) {{ :ensure => 'foo' }}
-
-          it 'should fail' do
-            expect {
-              should compile
-            }.to raise_error(/"foo" does not match/)
-          end
-        end # foo
-      end # ensure =>
-
-      context 'enable_tuning =>' do
-        context 'true' do
-          let(:params) {{ :enable_tuning => true }}
-
-          it { should contain_class('port389::tune') }
-          it_should_behave_like 'been_tuned'
-        end # true
-
-        context 'false' do
-          let(:params) {{ :enable_tuning => false }}
-
-          it { should_not contain_class('port389::tune') }
-        end # false
-
-        context '[]' do
-          let(:params) {{ :enable_tuning =>[] }}
-
-          it 'should fail' do
-            expect {
-              should compile
-            }.to raise_error(/is not a boolean/)
-          end
-        end # []
-      end # enable_tuning =>
-    end # on os version 7
+      let(:service_params) {{ :name => 'dirsrv.target' }}
+      include_examples 'port389 examples'
+    end # os version 7
   end # on osfamily RedHat
 
   describe 'on an unsupported osfamily' do
@@ -367,5 +206,4 @@ describe 'port389', :type => :class do
         to raise_error(/not supported on Debian/)
     end
   end # on an unsupported osfamily
-
 end
